6d4e80f206e34da1eb16278c46926c37913e8396
sa7936 <sa7936@rit.edu>
2024-02-08 17:48:08 -0500

    Commented code

--- centipede/centipede_play.py
+++ centipede/centipede_play.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 
 
 class Agent(object):
-
     """The world's simplest agent!"""
     action_number = 0
     elf_possible_colors = {
@@ -24,13 +23,19 @@ class Agent(object):
         self.action_space = action_space
 
     def get_location(self, img, color):
+        """
+        Returns the location of the
+        :param img:
+        :param color:
+        :return:
+        """
         result = zip(*np.where(img == color)[:2])
         locations = [x for x in result]
+        middle_el = len(locations)//2
         if locations:
             return locations[3]
         else:
             return None
-
 
     # You should modify this function
     # You may define additional functions in this
@@ -50,48 +55,52 @@ class Agent(object):
             https://gymnasium.farama.org/environments/atari/centipede/
         """
         new_array = observation.reshape((observation.shape[0] * observation.shape[1], 3))
-        var = {tuple(x) for x in new_array}
-        elf_color = None
-        centipede_color = None
-        if self.elf_possible_colors.get("level1") in var:
+        var = {tuple(x) for x in new_array}  # the array of colors
+        if self.elf_possible_colors.get("level1") in var:  # if condition to check if it's level 1 or 2
             elf_color = self.elf_possible_colors.get("level1")
             centipede_color = self.centipede_possible_colors.get("level1")
         else:
             elf_color = self.elf_possible_colors.get("level2")
             centipede_color = self.centipede_possible_colors.get("level2")
-        target_col = (184, 70, 162)
-        longest_seq_cent = None
-        elf_locations = None
         result2 = zip(*np.where(observation == elf_color)[:2])
-        elf_locations = [x for x in result2]
-        if not elf_locations: 
-            return 0
-        longest_seq_elf = self.longest_seq_color(elf_locations)
+        elf_locations = [x for x in result2]  # gets the array of everywhere the elf color was found
+        if not elf_locations:
+            return 0  # returns a no-op to know when the screen is blinking
+        longest_seq_elf = self.longest_seq_color(elf_locations)  # returns an image of just the elf on the screen
         result = zip(*np.where(observation == centipede_color)[:2])
         centipede_locations = list(result)
         if not centipede_locations:
-            return 0
-        longest_seq_cent = self.longest_seq_color(centipede_locations)
+            return 0  # returns a no-op if the centipede is not on the screen yet.
+        longest_seq_cent = self.longest_seq_color(centipede_locations)  # returns the longest sequence of centipede
+        # parts
 
-        centipede_location = self.get_location(longest_seq_cent, centipede_color)
-        elf_location = self.get_location(longest_seq_elf, elf_color)
-        offset = 0
+        centipede_location = self.get_location(longest_seq_cent, centipede_color)  # the actual tuple of the position
+        # of the centipede
+
+        elf_location = self.get_location(longest_seq_elf, elf_color) # the actual tuple of the position
+        # of the elf
+
 
         self.action_number += 1
 
         if centipede_location is not None and elf_location is not None:
             offset = centipede_location[1] - elf_location[1]
         else:
-            return 0 # return action number for no-op
+            return 0  # return action number for no-op
         if self.action_number % 5 == 0:
-            return 1 # return action number for shooting
+            return 1  # return action number for shooting
         if offset < 0:
-            return 4 # return action number for moving left
+            return 4  # return action number for moving left
         else:
-            return 3 # return action number for moving right
-
+            return 3  # return action number for moving right
 
     def longest_seq_color(self, locations):
+        """Finds the longest sequence of a particular color in an image given the locations
+           that color is found.
+        :param locations: The indexes a particular color is found at.
+        :return: An observation that is just the longest sequence of a color against a black backdrop.
+
+        """
         mask = np.zeros((210, 160), dtype=bool)
         for row, col in locations:
             mask[row, col] = True
@@ -113,6 +122,15 @@ class Agent(object):
         return largest_connected_region
 
     def depth_first_search(self, row, col, counter, mask, markers):
+        """
+        Recursively search for all the connected pixels that are one color and mark them the same integer
+        :param row: the row of the image currently on in the recursion
+        :param col: the col of the image currently on in the recursion
+        :param counter: the current int to mark the pixels
+        :param mask: the mask of the colors we are checking for
+        :param markers: a mirror of the mask that has the counters at the various pixel locations
+        :return: updated markers with the counters at the indexes that fulfilled the checks
+        """
         if mask[row, col] == True and markers[row, col] == 0 and (0 <= row < 210) and (0 <= col < 160):
             markers[row, col] = counter
             for i, j in ((row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)):
